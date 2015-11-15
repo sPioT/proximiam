591577a50a965a4bd4f4eae8d38d114ea49cf70a
PioT <stephane.piotrowski@gmail.com>
2015-11-15 16:04:51 +0100

    Gestion d'une connexion

--- js/proximiam.js
+++ js/proximiam.js
@@ -7,44 +7,58 @@
  */
 var proxiMiamApp = angular.module('proxiMiamApp', [
     // Dépendances du "module"
-    'eventList','timer'
+    'eventList','timer','menu'
 ]);
 
 /**
  * Déclaration du module eventList
  */
 var eventList = angular.module('eventList',[]);
+var menu = angular.module('menu',[]);
 
 /**
  * Contrôleur de l'application "ProxiaMiam"
  */
+
 eventList.controller('eventCtrl',
     function ($scope,$http,$filter) {
 
         $scope.isToday = function(dt){
-        	$scope.NewDt=$filter('date')(dt, 'shortDate');
-     		$scope.currentDate = $filter('date')(new Date(), 'shortDate');
-     		return $scope.NewDt==$scope.currentDate;
+            $scope.NewDt=$filter('date')(dt, 'shortDate');
+            $scope.currentDate = $filter('date')(new Date(), 'shortDate');
+            return $scope.NewDt==$scope.currentDate;
         }
 
         $scope.isPassed = function(dt){
-        	$scope.NewDt=$filter('date')(dt, 'yyyyMMddHHmmss');
-     		$scope.currentDate = $filter('date')(new Date(), 'yyyyMMddHHmmss');
+            $scope.NewDt=$filter('date')(dt, 'yyyyMMddHHmmss');
+            $scope.currentDate = $filter('date')(new Date(), 'yyyyMMddHHmmss');
 
-			if ( $scope.currentDate > $scope.NewDt) {
-	        	return true;
-	       	}
+            if ( $scope.currentDate > $scope.NewDt) {
+                return true;
+            }
         }
 
         $scope.refresh = function(){
-        	$http.get('../json/agenda.json').success(function(data){
-	           	$scope.agenda = data;
-	       	});
+            $http.get('../json/agenda.json?t='+new Date()).success(function(data){
+                $scope.agenda = data;
+            });
         }
 
         $scope.refresh();
     }
 
     
+
+);
+
+menu.controller('menuCtrl',
+    function ($scope,$http,$filter) {
+
+        $scope.connect = function(){
+            $http.get('../json/user.json?t='+new Date()).success(function(user){
+                $scope.user = user;
+            });
+        }
+    }    
 
 );
